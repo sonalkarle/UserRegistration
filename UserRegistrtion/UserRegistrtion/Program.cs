02b6c1a64a64fa12d9f14fc2f3392428be5b5b2c
sonal <sonalkarle01@gmail.com>
2021-02-11 23:01:03 +0530

    Validate the first name with first letter should be capitaland minumum 3 character

--- UserRegistrtion/UserRegistrtion/Program.cs
+++ UserRegistrtion/UserRegistrtion/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
-namespace UserRegistrtion
+namespace RegExPattern
 {
     class Program
     {
+        Regex firstnameregex = new Regex(@"^[A-Z][A-Za-z]{2,}$");
+
+        public void FirstNameValidation(string firstName)
+        {
+            Console.WriteLine("first name" + firstName);
+            if (firstnameregex.IsMatch(firstName))
+                Console.WriteLine("Valid first name");
+            else
+                Console.WriteLine("first name is not valid");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program person = new Program();
+            Console.WriteLine("Enter first name: ");
+            string firstName = Console.ReadLine();
+           
+            person.FirstNameValidation(firstName);
         }
+
+
     }
 }
